7d0d05f0d91a506e9ab5b996e81dc188985183d1
Josh Smith <git@breadlysm.com>
2020-08-05 23:06:18 -0400

    app can run in docker container.

--- pecoscraper/account.py
+++ pecoscraper/account.py
@@ -18,18 +18,17 @@ def login(driver):
     username.clear()
     username.send_keys(PECO_USERNAME)
     info('input username successfully')
-
     # Password
     password_xpath="/html/body/form/div[3]/div[4]/main/div/div/section/div/div[1]/div/div[1]/div/div/ng-form/div/exelon-decorator-simple/div/div/div/div[1]/div[2]/div/div/input"
     password = wait_until_exists(driver,password_xpath)
     password.clear()
     password.send_keys(PECO_PASSWORD)
     info('Input password successfully')
-
     # Click Signin
     # Add ability to check that the user is successsfully signed in. 
     info('Trying to submit sign in.')
-    button_xpath = "/html/body/form/div[3]/div[4]/main/div/div/section/div/div[1]/div/div[1]/div/div/ng-form/div/exelon-decorator-simple/div/div/div/div[1]/button"
+    button_xpath = "//button[contains(@class,'btn-primary')]"
+    #button_xpath = "/html/body/form/div[3]/div[4]/main/div/div/section/div/div[1]/div/div[1]/div/div/ng-form/div/exelon-decorator-simple/div/div/div/div[1]/button"
     signin = wait_until_exists(driver,button_xpath)
     signin.click()
     info("Signing in.")
