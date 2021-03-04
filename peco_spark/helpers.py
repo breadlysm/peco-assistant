c72c5fdc4522258e7fb85e1620bb88006940aab0
breadlysm <jbradly.smith@gmail.com>
2021-03-04 11:05:03 -0500

    added date handlers

--- peco_spark/helpers.py
+++ peco_spark/helpers.py
@@ -1,12 +1,18 @@
+import pytz
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import *
 from selenium import webdriver
 from datetime import datetime, date
+from pytz import timezone,utc
 import time
+import logging
+import sys
 import chromedriver_binary
 import os
+
+tz = timezone('US/Eastern')
 
 class Browser:
 
@@ -45,7 +51,32 @@ class Browser:
                     element = None
         return element
 
+def to_datetime(str,tz=None,local_tz=None,fmt="%a, %d %b %Y %H:%M:%S"):
+    """Returns datetime object from string
 
+    Args:
+        str (str): string representing a time, needs to be in valid format
+        tz (timezone, optional): pytz timezone obj to convert to. Defaults to None.
+        local_tz (timezone, optional): pytz tz obj string is in. Defaults to None
+        fmt (str, optional): Pass format for your str. Defaults to "%a, %d %b %Y %H:%M:%S".
+    Returns:
+        datetime: returns datetime obj
+    """
+    dt = datetime.strptime(str,fmt)
+    if tz == None or local_tz == None:
+        return dt
+    else:
+        dt = local_tz.localize(dt, is_dst=None)
+        dt = dt.astimezone(utc)
+        return dt
+
+def eastern():
+    return timezone('US/Eastern')
+
+
+def get_today():
+    today = datetime.datetime.now().isoformat()
+    return today
 
 def day(timestamp):
     return timestamp.strftime("%d")
@@ -58,3 +89,45 @@ def year(timestamp):
 
 def peco_date(timestamp):
     return timestamp.strftime("%Y/%m/%d")
+
+log_format = "%(asctime)s [%(levelname)s] %(message)s"
+
+def two_years():
+    return datetime.datetime.now() - datetime.timedelta(days=2*365)
+
+class Log:
+    def info(msg):
+        logging.basicConfig(
+            level=logging.INFO,
+            format=log_format,
+            handlers=[
+                #logging.FileHandler("log.log"),
+                logging.StreamHandler(sys.stdout)
+            ]
+        )
+        logging.info(msg)
+
+    def debug(msg):
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format=log_format,
+            handlers=[
+                #logging.FileHandler("debug.log"),
+                logging.StreamHandler(sys.stdout)
+            ]
+        )
+        print(msg)
+        logging.debug(msg)
+
+    def error(msg):
+        logging.basicConfig(
+            level=logging.ERROR,
+            format=log_format,
+            handlers=[
+                #logging.FileHandler("log.log"),
+                logging.StreamHandler(sys.stdout)
+            ]
+        )
+        logging.error(msg)
+log = Log()
+
