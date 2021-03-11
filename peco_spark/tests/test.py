ea87d29db6f1eb42cb0faf40aee1f6edfd360e4a
Josh Smith <git@breadlysm.com>
2021-03-10 23:57:22 -0500

    tests

--- peco_spark/tests/test.py
+++ peco_spark/tests/test.py
@@ -1,6 +1,22 @@
+from datetime import datetime,timedelta
+from peco_spark.helpers import peco_dates, to_utc
 from peco_spark.core import Account
+from peco_spark.database import Database
 
-account = Account()
-data = account.get_data('2021/2/01')
-print(data)
-print(account.kwh_cost)
+#account = Account()
+#data = account.get_data('2021/2/01')
+
+
+# days = peco_dates(to_utc(datetime.now() - timedelta(days=10)))
+# data = []
+# for i in days:
+#     d = account.get_data(i)
+#     if d is not None:
+#         data.append(d)
+# write_data = []
+# for day in data:
+#     for hour in day:
+#         write_data.append(hour)
+
+db = Database()
+print(db.last_write)
